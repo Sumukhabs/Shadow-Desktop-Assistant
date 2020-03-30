041ffc6b1f733b2475695de8b4a3acc497f1e409
Sumukha B S <sumukhabs5@gmail.com>
2020-03-30 08:18:16 +0530

    Add files via upload

--- shadow.py
+++ shadow.py
@@ -1,21 +1,104 @@
 import pyttsx3
 import speech_recognition as sr
-import datetime
+import datetime 
 import wikipedia
 import webbrowser
 import os
+os.add_dll_directory(r'C:\\Program Files\\VideoLAN\\VLC')
 import smtplib
 import requests
 import random
 from random import randrange
+import email
+import imaplib
+import numpy as np
+from youtube_search import YoutubeSearch
+import json
+import pafy,vlc,youtube_dl
+from datetime import date
+from sound import Sound
 
 
-maildict={"antony":"antonys_mail_id"} #Sending mail Id To whom you want to send a mail 
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 #print(voices[4].id)
 engine.setProperty('voice', voices[4].id)  #  voice id  (assigning a voice)
+
+maildict={"antony":"sumukhabs5@gmail.com"}
+username = 'sumukhabs31@gmail.com'
+password='gotohell77@'
+imapurl="imap.gmail.com"
+attachment_dir="C:\\Users\\Sumukha\\Documents\\Emails"
+
+
+#def getbody(msg):
+    #if msg.is_multipart():
+        #msgg=getbody(msg.get_payload(0))
+        #return msgg
+
+        #for payload in msg.get_payload():
+            #print(payload.get_payload())
+
+        #msg_string=msg.decode('utf-8')
+        # converts byte literal to string removing b''
+        #email_message = email.message_from_string(msg_string)
+        #print(email_message)
+        
+    #else:
+        #return msg.get_payload(None,True)
+        #speech(msg.get_payload())
+
+
+def readmail(): 
+        mail = imaplib.IMAP4_SSL(imapurl)
+        mail.login(username,password)
+        mail.list()
+        mail.select("INBOX")
+        type, data = mail.search(None, 'ALL')
+        mailids=data[0]
+        id_list = mailids.split()
+        latest_email = (id_list[-1])
+        result, data=mail.fetch(latest_email,'(RFC822)')
+
+        msg = email.message_from_bytes(data[0][1])
+        email_subject = msg['subject']
+        email_from = msg['from']
+        
+        speech('The last Mail you receved is From : ' + email_from )
+        speech('Subject is: ' + email_subject)
+        
+        #raw=email.message_from_bytes(data[0][1])
+        raw_email=data[0][1]
+        raw_email_string=raw_email.decode('utf-8')
+        raw=email.message_from_string(raw_email_string)
+        for part in raw.walk():
+            if part.get_content_type() == "text/plain": # ignore attachments/html
+                body = part.get_payload(decode=True)
+                body2=body.decode('utf-8')
+                speech("message is: " + body2)
+
+            else:
+                continue
+
+        for part in msg.walk():
+            if part.get_content_maintype()=='multipart':
+                continue 
+
+            if part.get('Content-Disposition') is None:
+                continue
+
+            fileName = part.get_filename()
+
+            if bool(fileName):
+                filePath = os.path.join(attachment_dir, fileName)
+                with open(filePath,'wb') as f:
+                    f.write(part.get_payload(decode=True))
+                    speech("This Mail contains an attachment named "+ fileName)
+                    speech("It is saved in emails in Document Folder")
+
+        
+        
 
 
 def speech(audio):
@@ -49,7 +132,7 @@ def takeCommand():
         print(f"user said: {query}\n")
     except Exception as e:
         print(e)
-        speech("Sorry , can you repeat?")
+        print("Say that again..")
         return "None"
     return query
 
@@ -57,8 +140,8 @@ def sendMail(mailId,message):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('Yourmailid', 'e-mail password')
-    server.sendmail('youremailid',mailId , message)
+    server.login(username, password)
+    server.sendmail(username,mailId , message)
     server.close()
 
 def givenews():
@@ -81,6 +164,9 @@ def givenews():
         else:
             speech("Next news :")
         speech(to_speak)
+
+def giveweather():
+    return
 
 
 def clear():
@@ -111,7 +197,7 @@ if __name__ == "__main__":
             webbrowser.open("stackoverflow.com")
 
         elif 'play music' in query or 'play songs' in query:
-            music_dir='path for the songs' #you must provide the correct path
+            music_dir='C:\\Users\\Sumukha\\Music'
             sno=len(music_dir)
             rint=randrange(0,sno-1)
             songs=os.listdir(music_dir)
@@ -121,9 +207,13 @@ if __name__ == "__main__":
             time= datetime.datetime.now().strftime("%H:%M:%S")
             speech(f" Sir the time is {time}")
 
-        elif 'open code editor' in query:
-           atomPath='path for the code editor' #please specify the correct target or path in the properties
+        elif 'open atom editor' in query:
+           atomPath='C:\\Users\\Sumukha\\AppData\\Local\\atom\\atom.exe'
            os.startfile(atomPath)
+
+        elif 'open spotify' in query:
+           spotifyPath='C:\\Users\\Sumukha\\AppData\\Roaming\\Spotify\\Spotify.exe'
+           os.startfile(spotifyPath)
 
         elif 'send email' in query:
             try:
@@ -148,5 +238,66 @@ if __name__ == "__main__":
             webbrowser.open(f"https://www.google.com/search?&q={query}")
             speech(wikipedia.summary(query, sentences=2))
 
+       
+        elif 'gmail' in query or 'mail' in query:
+            webbrowser.open("https://mail.google.com/mail/u/2/#inbox")
+            readmail()
+
+        elif 'youtube' in query:
+            results = YoutubeSearch(query, max_results=10).to_json()
+            ress=json.loads(results)
+            p=ress['videos']
+            q=p[0]
+            r=q['id']
+            webbrowser.open("http://www.youtube.com/watch?v=" + r)
+            
+            #url="http://www.youtube.com/watch?v=" + r
+            #video=pafy.new(url)
+            #best=video.getbest()
+            #media=vlc.MediaPlayer(best.url)
+            #media.play()
+
+        elif 'tell me a joke' in query:
+            res = requests.get(
+                'https://icanhazdadjoke.com/',
+                headers={"Accept":"application/json"}
+                )
+            if res.status_code == requests.codes.ok:
+                speech(str(res.json()['joke']))
+            else:
+                speech('oops!I ran out of jokes')
+
+        elif "close browser" in query:
+            #os.system("taskkill /im firefox.exe /f")
+            os.system("taskkill /im chrome.exe /f")
+
+        elif "weather" in query:
+            giveweather()
+
         elif 'quit' in query:
             exit(0)
+
+        elif 'restart pc' in query:
+            os.system("shutdown /r /t 1")
+
+        elif 'shutdown pc' in query:
+            os.system("shutdown /s /t 1")
+
+
+        elif 'mute' in query or 'unmute' in query:
+            Sound.mute()
+
+        elif 'increase volume' in query:
+            Sound.volume_up()
+
+        elif 'decrease volume' in query:
+            Sound.volume_down()
+
+        elif 'set maximum volume' in query:
+            Sound.volume_min()
+
+        elif 'set volume to zero' in query:
+            Sound.volume_max()
+
+
+
